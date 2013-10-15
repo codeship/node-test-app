c84276e22857169da7773b95670d1a6ea1f204fa
Florian Motlik <flo@codeship.io>
2013-10-14 22:41:15 -0700

    Edit text of main page

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
 
   // write some content to the browser that your user will see
-  res.write('<h1>Codeship test</h1>');
+  res.write('<h1>Hello Dotcloud</h1>');
 
   // close the response
   res.end();
